fd88cb8dfd93ecb5776f731d1aff16fed8cedb89
Travis Phelps <tzphelps@syr.edu>
2024-12-08 22:30:54 +0000

    Finalized tables

--- quickstart/models/dim_user.sql
+++ quickstart/models/dim_user.sql
@@ -8,7 +8,8 @@ SELECT {{ dbt_utils.generate_surrogate_key(['u.user_id']) }} as userkey,
     u.user_id as userid, u.user_firstname as userfirstname, u.user_lastname as userlastname, 
     concat(u.user_firstname ,' ' , u.user_lastname) as usernamefirstlast,
     concat(u.user_lastname ,', ' , u.user_firstname) as usernamelastfirst,
-    u.user_email as useremail, z.zip_city as usercity, z.zip_state as userstate, z.zip_code as userzipcode
+    u.user_email as useremail, z.zip_city as usercity, z.zip_state as userstate, z.zip_code as userzipcode,
+    z.zip_lat, z.zip_lng
 FROM stg_users u
 LEFT JOIN stg_zip_codes z ON u.user_zip_code = z.zip_code
 
